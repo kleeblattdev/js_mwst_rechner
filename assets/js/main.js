26878191bcdb4dd1e43ca15bf48a3552186bbc30
karinlee <karinlee92@gmail.com>
2023-01-26 17:26:39 +0100

    better overview in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,8 @@
 //Input
 const nettoBrutto = document.querySelector("#nettoBrutto");
 const bruttoNetto = document.querySelector("#bruttoNetto");
-
 const mwst19 = document.querySelector("#mwst19");
 const mwst7 = document.querySelector("#mwst7");
-
 const labelChange = document.querySelector("#labelChange");
 const nettoBetrag = document.querySelector("#nettoBetrag");
 
@@ -12,8 +10,7 @@ const nettoBetrag = document.querySelector("#nettoBetrag");
 const resultText = document.querySelector("#resultText");
 const mwstResult = document.querySelector("#mwstResult");
 const bruttoResult =document.querySelector("#bruttoResult");
-
-//functions variables
+//functions
 
 const nettoToBrutto = () => {
     resultText.innerHTML = "Bruttobetrag (Endpreis)";
@@ -27,30 +24,10 @@ const bruttoToNetto = () =>{
 
 const calculate = () => {
     let input = Number(nettoBetrag.value);
-
     let output1 = "";
     let output2 = "";
-
-/*     if(nettoBrutto.checked === true && mwst19.checked === true){
-        output1 = input * 0.19;
-        output2 = output1 + input;
-    }
-    else if(nettoBrutto.checked === true && mwst7.checked === true){
-        output1 = input * 0.07;
-        output2 = output1 + input;
-    }
-    else if (bruttoNetto.checked === true && mwst19.checked === true){
-        output1 = input -(input / 1.19);
-        output2 = input / 1.19;
-    }
-    else if (bruttoNetto.checked === true && mwst7.checked === true){
-        output1 = input - (input / 1.07);
-        output2 = input / 1.07 ;
-    }
-    else{}
-    mwstResult.innerHTML = output1.toFixed(2);
-    bruttoResult.innerHTML = output2.toFixed(2); */
     let x = "";
+
     if(mwst19.checked === true ){
         x = 1.19 ;
     }
@@ -69,6 +46,27 @@ const calculate = () => {
 
     mwstResult.innerHTML = output1.toFixed(2);
     bruttoResult.innerHTML = output2.toFixed(2);
+
+    /*  Solution 1
+    if(nettoBrutto.checked === true && mwst19.checked === true){
+        output1 = input * 0.19;
+        output2 = output1 + input;
+    }
+    else if(nettoBrutto.checked === true && mwst7.checked === true){
+        output1 = input * 0.07;
+        output2 = output1 + input;
+    }
+    else if (bruttoNetto.checked === true && mwst19.checked === true){
+        output1 = input -(input / 1.19);
+        output2 = input / 1.19;
+    }
+    else if (bruttoNetto.checked === true && mwst7.checked === true){
+        output1 = input - (input / 1.07);
+        output2 = input / 1.07 ;
+    }
+    else{}
+    mwstResult.innerHTML = output1.toFixed(2);
+    bruttoResult.innerHTML = output2.toFixed(2); */
 }
 
 
